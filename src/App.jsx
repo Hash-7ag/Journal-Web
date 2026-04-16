8d701e7dc3260333bc6da0a63248f0279e09eacd
Nan0-S <nan.fgbs@gmail.com>
2026-04-16 22:03:32 +0400

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+
+   return (
+      <>
+         <div className='container mx-auto flex justify-center items-center'>
+
+         </div>
+      </>
+   )
+}
+
+export default App
